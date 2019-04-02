b05ff47a9584000da4166eb9f11ae7f240b58737
jhvics1 <jhvics1@gmail.com>
2019-04-02 20:57:58 +0900

    Add script for downloading datasets
    - download_flow : optical flow
    - download_rgb : rgb
    
    Signed-off-by: jhvics1 <jhvics1@gmail.com>

---
+++ download_flow_dataset.sh
@@ -0,0 +1,6 @@
+wget http://ftp.tugraz.at/pub/feichtenhofer/tsfusion/data/ucf101_tvl1_flow.zip.001
+wget http://ftp.tugraz.at/pub/feichtenhofer/tsfusion/data/ucf101_tvl1_flow.zip.002
+wget http://ftp.tugraz.at/pub/feichtenhofer/tsfusion/data/ucf101_tvl1_flow.zip.003
+
+cat ucf101_tvl1_flow.zip* > ucf101_tvl1_flow.zip
+unzip ucf101_tvl1_flow.zip
